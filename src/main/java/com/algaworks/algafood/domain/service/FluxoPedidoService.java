ca4009c14c7815d5c8b44bfaaddca90cbe5e42fa
Matheus Barcelos <matheus.sl.dh@gmail.com>
2021-12-13 08:13:59 -0300

    Preparação para envio de emails com sendGrid

--- src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -1,6 +1,5 @@
 package com.algaworks.algafood.domain.service;
 
-import com.algaworks.algafood.domain.model.Pedido;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,21 +10,30 @@ public class FluxoPedidoService {
 
     private EmissaoPedidoService emissaoPedidoService;
 
+    private EnvioEmailService envioEmailService;
+
     @Transactional
-    public void confirmar(String codigoPedido){
-        Pedido pedido = emissaoPedidoService.buscarOuFalhar(codigoPedido);
+    public void confirmar(String codigoPedido) {
+        var pedido = emissaoPedidoService.buscarOuFalhar(codigoPedido);
         pedido.confirmar();
+        var mensagem = EnvioEmailService.Mensagem.builder()
+                .assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
+                .corpo("O pedido <strong>" + pedido.getCodigo() + "</strong> foi confirmado!")
+                .destinatario(pedido.getCliente().getEmail())
+                .build();
+
+        envioEmailService.enviar(mensagem);
     }
 
     @Transactional
-    public void entregar(String codigoPedido){
-        Pedido pedido = emissaoPedidoService.buscarOuFalhar(codigoPedido);
+    public void entregar(String codigoPedido) {
+        var pedido = emissaoPedidoService.buscarOuFalhar(codigoPedido);
         pedido.entregar();
     }
 
     @Transactional
-    public void cancelar(String codigoPedido){
-        Pedido pedido = emissaoPedidoService.buscarOuFalhar(codigoPedido);
+    public void cancelar(String codigoPedido) {
+        var pedido = emissaoPedidoService.buscarOuFalhar(codigoPedido);
         pedido.cancelar();
     }
 }
